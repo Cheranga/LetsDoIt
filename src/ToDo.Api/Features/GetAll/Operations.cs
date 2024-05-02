0b42682b13841dcae1e060e1e9f0c48544481f41
Cheranga Hatangala <cheranga@gmail.com>
2024-05-03 09:10:19 +1000

    chore: adding distributed cache

--- src/ToDo.Api/Features/GetAll/Operations.cs
+++ src/ToDo.Api/Features/GetAll/Operations.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.ObjectModel;
-using System.Net;
+﻿using System.Net;
+using System.Text;
+using System.Text.Json;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
 using ToDo.Api.Features.SearchById;
 using ToDo.Api.Infrastructure.DataAccess;
 
@@ -9,18 +11,10 @@ namespace ToDo.Api.Features.GetAll;
 
 internal static class Operations
 {
-    private static Results<ProblemHttpResult, NoContent, Ok<TodoListResponse>> ToResponse(this List<TodoDataModel> todos) =>
-        todos.Any()
-            ? TypedResults.Ok(
-                new TodoListResponse(
-                    new ReadOnlyCollection<TodoResponse>(
-                        todos.Select(x => new TodoResponse(x.Id, x.Title, x.Description, x.DueDate)).ToList()
-                    )
-                )
-            )
-            : TypedResults.NoContent();
+    private static readonly string CacheKey = $"{typeof(Operations).FullName}-AllTasks";
 
     public static async ValueTask<Results<ProblemHttpResult, NoContent, Ok<TodoListResponse>>> ExecuteAsync(
+        [FromServices] IDistributedCache cache,
         [FromServices] IQueryHandler<SearchAllQuery, List<TodoDataModel>> queryHandler,
         [FromServices] ILogger<Program> logger,
         CancellationToken token = new()
@@ -28,8 +22,30 @@ internal static class Operations
     {
         try
         {
-            var results = await queryHandler.QueryAsync(new SearchAllQuery(), token);
-            return (results ?? new List<TodoDataModel>()).ToResponse();
+            var rawData = await cache.GetAsync(CacheKey, token);
+            if (rawData != null)
+            {
+                using var memoryStream = new MemoryStream(rawData);
+                var response = (await JsonSerializer.DeserializeAsync<TodoListResponse>(memoryStream, cancellationToken: token))!;
+                return response.Tasks.Any() ? TypedResults.Ok(response) : TypedResults.NoContent();
+            }
+
+            var results = await queryHandler.QueryAsync(new SearchAllQuery(), token) ?? new List<TodoDataModel>();
+            var todoListResponse = new TodoListResponse
+            {
+                Tasks =
+                [
+                    ..results.Select(x => new TodoResponse(x.Id, x.Title, x.Description, x.DueDate)).ToList()
+                ]
+            };
+
+            await cache.SetAsync(
+                CacheKey,
+                Encoding.UTF8.GetBytes(JsonSerializer.Serialize(todoListResponse)),
+                new DistributedCacheEntryOptions {AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(30)},
+                token
+            );
+            return todoListResponse.Tasks.Any() ? TypedResults.Ok(todoListResponse) : TypedResults.NoContent();
         }
         catch (Exception exception)
         {
--- src/ToDo.Api/Features/GetAll/SearchAllQuery.cs
+++ src/ToDo.Api/Features/GetAll/SearchAllQuery.cs
@@ -1,16 +1,53 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
 using ToDo.Api.Infrastructure.DataAccess;
 
 namespace ToDo.Api.Features.GetAll;
 
 public record SearchAllQuery : IQuery
 {
-    internal record Handler(TodoDbContext Context) : IQueryHandler<SearchAllQuery, List<TodoDataModel>>
+    private static readonly string CacheKey = $"{typeof(SearchAllQuery).FullName}-GetAll";
+
+    private static readonly JsonSerializerOptions SerializerOptions = new()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+    };
+
+    private static readonly DistributedCacheEntryOptions CacheOptions = new()
+    {
+        AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(30)
+    };
+
+    internal record Handler(
+        TodoDbContext Context,
+        IDistributedCache Cache) : IQueryHandler<SearchAllQuery, List<TodoDataModel>>
     {
         public async ValueTask<List<TodoDataModel>?> QueryAsync(SearchAllQuery query, CancellationToken token)
         {
-            var todos = await Context.Todos.AsNoTracking().ToListAsync(token);
-            return todos;
+            var tasks = new List<TodoDataModel>();
+            var rawData = await Cache.GetAsync(CacheKey, token);
+            if (rawData != null)
+            {
+                using var memoryStream = new MemoryStream(rawData);
+                tasks = await JsonSerializer.DeserializeAsync<List<TodoDataModel>>(memoryStream, SerializerOptions, token);
+            }
+
+            if (tasks == null || !tasks.Any())
+            {
+                tasks = await Context.Todos.AsNoTracking().ToListAsync(token);
+            }
+
+            using var stream = new MemoryStream();
+            await JsonSerializer.SerializeAsync(stream, tasks, SerializerOptions, token);
+            stream.Position = 0;
+            using var reader = new BinaryReader(stream);
+            var bytes = reader.ReadBytes((int) stream.Length);
+            await Cache.SetAsync(CacheKey, bytes, CacheOptions, token);
+
+            return tasks;
         }
     }
 }

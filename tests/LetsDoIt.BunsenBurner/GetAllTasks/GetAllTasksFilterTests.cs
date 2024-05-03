5fc32659b5a8991398a5241cce0ce0965a7c980b
Cheranga Hatangala <cheranga@gmail.com>
2024-05-04 08:03:11 +1000

    chore: separating arranges so it's more readable

--- tests/LetsDoIt.BunsenBurner/GetAllTasks/GetAllTasksFilterTests.cs
+++ tests/LetsDoIt.BunsenBurner/GetAllTasks/GetAllTasksFilterTests.cs
@@ -1,5 +1,4 @@
 ﻿using System.Net;
-using System.Text;
 using System.Text.Json;
 using AutoFixture;
 using FluentAssertions;
@@ -9,9 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
-using ToDo.Api.Features.Create;
 using ToDo.Api.Features.GetAll;
-using ToDo.Api.Features.SearchById;
 using ToDo.Api.Infrastructure.DataAccess;
 using static BunsenBurner.Aaa;
 
@@ -19,28 +16,36 @@ namespace LetsDoIt.BunsenBurner.GetAllTasks;
 
 public class GetAllTasksFilterTests(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
 {
-    [Fact]
+    [Fact(DisplayName = "When cached, the tasks must be returned from the cache")]
     public async Task GetAllTasksWhenCached()
     {
         await Arrange(() =>
             {
-                return factory
-                    .WithWebHostBuilder(builder =>
+                return factory.WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureTestServices(services =>
+                    {
+                        var dbContextDescriptor = services.SingleOrDefault(d =>
+                            d.ServiceType == typeof(DbContextOptions<TodoDbContext>)
+                        );
+                        if (dbContextDescriptor != null)
+                        {
+                            services.Remove(dbContextDescriptor);
+                        }
+
+                        services.AddDbContext<TodoDbContext>(optionsBuilder =>
+                        {
+                            optionsBuilder.UseInMemoryDatabase(Guid.NewGuid().ToString("N"));
+                        });
+                    });
+                });
+            })
+            .And(f =>
+            {
+                return f.WithWebHostBuilder(builder =>
                     {
                         builder.ConfigureTestServices(services =>
                         {
-                            var dbContextDescriptor = services.SingleOrDefault(
-                                d => d.ServiceType ==
-                                     typeof(DbContextOptions<TodoDbContext>));
-                            if (dbContextDescriptor != null)
-                            {
-                                services.Remove(dbContextDescriptor);
-                            }
-
-                            services.AddDbContext<TodoDbContext>(optionsBuilder =>
-                            {
-                                optionsBuilder.UseInMemoryDatabase(Guid.NewGuid().ToString("N"));
-                            });
                             var tasks = new Fixture().CreateMany<TodoDataModel>().ToList();
                             var mockedCache = new Mock<IDistributedCache>();
                             mockedCache
@@ -68,7 +73,10 @@ public class GetAllTasksFilterTests(WebApplicationFactory<Program> factory) : IC
             .And(async responses =>
             {
                 var responseContent2 = await responses.httpResponse2.Content.ReadAsStringAsync();
-                var todoListResponse = JsonSerializer.Deserialize<TodoListResponse>(responseContent2, Constants.SerializerOptions);
+                var todoListResponse = JsonSerializer.Deserialize<TodoListResponse>(
+                    responseContent2,
+                    Constants.SerializerOptions
+                );
                 todoListResponse.Should().NotBeNull();
                 todoListResponse!.Tasks.Should().NotBeNull().And.HaveCount(3);
             });
